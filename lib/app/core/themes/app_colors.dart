f74691cc526cbe1b2769858fb24dbab53f3fb852
msinggiadityaramadhan <msinggiadityaramadhan@gmail.com>
2024-11-06 12:06:26 +0000

    chore: init project

---
+++ lib/app/core/themes/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color blackColor = Color.fromRGBO(0, 0, 0, 1);
+Color greenColor = Color.fromRGBO(229, 255, 127, 1);
+Color yellowColor = Color.fromRGBO(255, 219, 86, 1);
+Color pinkColor = Color.fromRGBO(255, 171, 156, 1);
+Color redColor = Color.fromRGBO(254, 116, 116, 1);
+Color textColor = Color.fromRGBO(63, 56, 73, 1);
---
+++ lib/app/core/themes/app_textstyles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle regularTextStyle =
+    GoogleFonts.inter(fontSize: 10, fontWeight: FontWeight.w400);
+
+TextStyle titleSTextStyle =
+    GoogleFonts.inter(fontSize: 20, fontWeight: FontWeight.w700);
+TextStyle titleMTextStyle =
+    GoogleFonts.inter(fontSize: 24, fontWeight: FontWeight.w700);
+TextStyle titleLTextStyle =
+    GoogleFonts.inter(fontSize: 32, fontWeight: FontWeight.w700);
+TextStyle titleXLTextStyle =
+    GoogleFonts.inter(fontSize: 40, fontWeight: FontWeight.w700);
+
+TextStyle bodyMTextStyle =
+    GoogleFonts.inter(fontSize: 12, fontWeight: FontWeight.w400);
+TextStyle bodyLTextStyle =
+    GoogleFonts.inter(fontSize: 14, fontWeight: FontWeight.w400);
+TextStyle bodyBaseTextStyle =
+    GoogleFonts.inter(fontSize: 16, fontWeight: FontWeight.w500);
